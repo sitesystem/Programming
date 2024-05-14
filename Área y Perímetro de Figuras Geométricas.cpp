37014465339aabb997bbeb7ece83faf75ffc2757
sitesystem <l_aloaxa05@hotmail.com>
2024-05-14 01:40:00 -0600

    ...

---
+++ Área y Perímetro de Figuras Geométricas.cpp
@@ -0,0 +1,23 @@
+// Área y Perímetro de Figuras Geométricas
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<math.h>
+#include<iostream>
+using namespace std;
+
+main()
+{
+	int opcion;
+
+	printf(".: MENÚ :.\n");
+	cout << "1.- Círculo" << endl;
+	cout << "2.- Cuadrado" << endl;
+	cout << "3.- Triángulo" << endl;
+	printf("Selecciona una opción: ");
+	scanf("%d", &opcion);
+
+	system("Pause");
+	// getchar();
+}
